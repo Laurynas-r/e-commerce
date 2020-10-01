49c61eb5b0f0db118ca5ee66db07baabe4bdc4ee
Laurynas-r <laurynasnau@gmail.com>
2020-10-01 20:18:04 +0200

    Quick fix to add styles to collections overview component

--- src/components/collection-overview/collections-overview.component.jsx
+++ src/components/collection-overview/collections-overview.component.jsx
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 import { selectCollections } from '../../redux/shop/shop.selectors';
 import { createStructuredSelector } from 'reselect';
 
+import './collections-overview.styles.scss';
+
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 const CollectionsOverview = ({collections}) => (
-    <div>
+    <div className="collections-overview">
         {
             collections.map(({ id, ...otherCollectionProps }) => (
                 <CollectionPreview key={id} {...otherCollectionProps} />
